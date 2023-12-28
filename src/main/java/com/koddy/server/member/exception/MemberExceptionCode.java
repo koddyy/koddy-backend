d4d241da321481b6374921f440ae589f7905dea8
SeoJiWon <sjiwon4491@gmail.com>
2023-12-28 21:14:02 +0900

    feat: add (Member, Mentor, Mentee) Repository

--- src/main/java/com/koddy/server/member/exception/MemberExceptionCode.java
+++ src/main/java/com/koddy/server/member/exception/MemberExceptionCode.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Getter
 @RequiredArgsConstructor
@@ -18,6 +19,9 @@ public enum MemberExceptionCode implements KoddyExceptionCode {
     PERIOD_MUST_EXISTS(BAD_REQUEST, "MEMBER_005", "커피챗 시작/종료 시간을 선택해주세요."),
     START_END_MUST_BE_ALIGN(BAD_REQUEST, "MEMBER_006", "시작 시간이 종료 시간 이후가 될 수 없습니다."),
     SCHEDULE_MUST_BE_EXISTS(BAD_REQUEST, "MEMBER_007", "커피챗 시간대를 선택해주세요."),
+    MEMBER_NOT_FOUND(NOT_FOUND, "MEMBER_008", "사용자 정보가 존재하지 않습니다."),
+    MENTOR_NOT_FOUND(NOT_FOUND, "MEMBER_009", "멘토 정보가 존재하지 않습니다."),
+    MENTEE_NOT_FOUND(NOT_FOUND, "MEMBER_010", "멘티 정보가 존재하지 않습니다."),
     ;
 
     private final HttpStatus status;
