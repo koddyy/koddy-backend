cbf9bbe9afb6fdbc45aa29b401ef930db270e7de
SeoJiWon <sjiwon4491@gmail.com>
2023-12-28 16:06:58 +0900

    feat: add `@UseCase` annotation

---
+++ src/main/java/com/koddy/server/global/annotation/UseCase.java
@@ -0,0 +1,14 @@
+package com.koddy.server.global.annotation;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Component
+public @interface UseCase {
+}
