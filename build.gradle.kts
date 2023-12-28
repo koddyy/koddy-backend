0771edfd28cdbe840127e02ac3f80136204e67e4
sjiwon <sjiwon4491@gmail.com>
2023-12-28 15:41:30 +0900

    [Infra] 프로젝트 기본 구조를 세팅한다 (#5)
    * rename: ServerApplication -> KoddyApplication
    
    * chore: add gradle.properties
    
    * chore: `settings.gradle.kts` gradle.properties 버전 관리
    
    * chore: `build.gradle.kts` 의존성, Rest Docs 파이프라인 구현
    
    * chore: update .gitignore
    
    * chore: add application.yml
    
    * style: optimize import, code cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,155 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
-	java
-	id("org.springframework.boot") version "3.2.1"
-	id("io.spring.dependency-management") version "1.1.4"
+    java
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    id("org.asciidoctor.jvm.convert")
 }
 
-group = "com.koddy"
-version = "0.0.1-SNAPSHOT"
+group = "${property("projectGroup")}"
+version = "${property("applicationVersion")}"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.valueOf("VERSION_${property("javaVersion")}")
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // Spring Web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.security:spring-security-crypto")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    // Data
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.mysql:mysql-connector-j")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-mysql")
+
+    // Query Builder
+    implementation("com.querydsl:querydsl-jpa:${property("queryDslVersion")}:jakarta")
+    annotationProcessor("com.querydsl:querydsl-apt:${property("queryDslVersion")}:jakarta")
+    annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+    annotationProcessor("jakarta.persistence:jakarta.persistence-api")
+
+    // Cloud Infra
+    implementation("io.awspring.cloud:spring-cloud-aws-starter:${property("awspringVersion")}")
+    implementation("io.awspring.cloud:spring-cloud-aws-starter-s3:${property("awspringVersion")}")
+
+    // Monitoring
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:${property("jwtTokenVersion")}")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:${property("jwtTokenVersion")}")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:${property("jwtTokenVersion")}")
+
+    // Swagger
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${property("swaggerVersion")}")
+
+    // Lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testCompileOnly("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
+
+    // Guava
+    implementation("com.google.guava:guava:${property("guavaVersion")}-jre")
+
+    // Slack API
+    implementation("com.slack.api:slack-api-client:${property("slackApiVersion")}")
+
+    // p6spy
+    implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:${property("p6spyVersion")}")
+
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // Spring REST Docs (With MockMvc)
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+
+    // RestAssured (E2E Test)
+    testImplementation("io.rest-assured:rest-assured:${property("restAssuredVersion")}")
+
+    // TestContainers
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:mysql")
+    testImplementation("org.flywaydb.flyway-test-extensions:flyway-spring-test:${property("flywayTestExtensionVersion")}")
+}
+
+val queryDslTypeDir: String = "src/main/generated"
+
+tasks.withType<JavaCompile>().configureEach {
+    options.generatedSourceOutputDirectory = file(queryDslTypeDir)
+}
+
+sourceSets {
+    getByName("main").java.srcDirs(queryDslTypeDir)
+}
+
+tasks.named("clean") {
+    doLast {
+        file(queryDslTypeDir).deleteRecursively()
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
+}
+
+val asciidoctorExt: Configuration by configurations.creating
+dependencies {
+    asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor")
+}
+
+val snippetsDir: File by extra { file("build/generated-snippets") }
+tasks {
+    test {
+        outputs.dir(snippetsDir)
+    }
+
+    asciidoctor {
+        configurations(asciidoctorExt.name)
+        dependsOn(test)
+
+        doFirst {
+            delete(file("src/main/resources/static/docs"))
+        }
+
+        inputs.dir(snippetsDir)
+
+        doLast {
+            copy {
+                from("build/docs/asciidoc")
+                into("src/main/resources/static/docs")
+            }
+        }
+    }
+
+    build {
+        dependsOn(asciidoctor)
+    }
+}
+
+tasks.named<Jar>("jar") {
+    enabled = false
+}
+
+tasks.named<BootJar>("bootJar") {
+    archiveBaseName = "Koddy"
+    archiveFileName = "Koddy.jar"
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,17 @@
-rootProject.name = "koddy-backend"
+rootProject.name = "koddy-server"
+
+pluginManagement {
+    val springBootVersion: String by settings
+    val springDependencyManagementVersion: String by settings
+    val asciidoctorJvmConvertVersion: String by settings
+
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "org.springframework.boot" -> useVersion(springBootVersion)
+                "io.spring.dependency-management" -> useVersion(springDependencyManagementVersion)
+                "org.asciidoctor.jvm.convert" -> useVersion(asciidoctorJvmConvertVersion)
+            }
+        }
+    }
+}
