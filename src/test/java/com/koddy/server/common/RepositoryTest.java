426d04a41387e230c7fb015a2ea7223cf4646c54
SeoJiWon <sjiwon4491@gmail.com>
2023-12-28 20:29:42 +0900

    refactor: RepositoryTest Query 관련 `@Import` 추가

--- src/test/java/com/koddy/server/common/RepositoryTest.java
+++ src/test/java/com/koddy/server/common/RepositoryTest.java
@@ -1,14 +1,18 @@
 package com.koddy.server.common;
 
+import com.github.gavlyukovskiy.boot.jdbc.decorator.p6spy.P6SpyConfiguration;
 import com.koddy.server.common.config.MySqlTestContainersExtension;
+import com.koddy.server.global.config.QueryDslConfig;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 @Tag("Repository")
 @DataJpaTest(showSql = false)
 @ExtendWith(MySqlTestContainersExtension.class)
+@Import({QueryDslConfig.class, P6SpyConfiguration.class})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public abstract class RepositoryTest {
 }
