9b83ac92e58ad87fafb6779ee421c93e07e63680
SeoJiWon <sjiwon4491@gmail.com>
2023-12-28 21:25:23 +0900

    feat: 토큰 관리 도메인 Token 구현
    - Flyway Script는 이전에 백업한 데이터 전부 적용한거 -> member_token 스키마 분리 후 DB Flyway 초기화

--- src/main/resources/db/migration/V1__init_about_member.sql
+++ src/main/resources/db/migration/V1__init_about_member.sql
@@ -50,16 +50,6 @@ CREATE TABLE IF NOT EXISTS member_role
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS member_token
-(
-    id               BIGINT AUTO_INCREMENT PRIMARY KEY,
-    member_id        BIGINT       NOT NULL UNIQUE,
-    refresh_token    VARCHAR(250) NOT NULL UNIQUE,
-    created_at       DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    last_modified_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
-
 CREATE TABLE IF NOT EXISTS mentor_chat_time
 (
     id               BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -89,11 +79,6 @@ ALTER TABLE available_language
 
 ALTER TABLE member_role
     ADD CONSTRAINT fk_member_role_member_id_from_member
-        FOREIGN KEY (member_id)
-            REFERENCES member (id);
-
-ALTER TABLE member_token
-    ADD CONSTRAINT fk_member_token_member_id_from_member
         FOREIGN KEY (member_id)
             REFERENCES member (id);
 
---
+++ src/test/java/com/koddy/server/auth/domain/model/TokenTest.java
@@ -0,0 +1,28 @@
+package com.koddy.server.auth.domain.model;
+
+import com.koddy.server.common.ParallelTest;
+import com.koddy.server.member.domain.model.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.koddy.server.common.fixture.MentorFixture.MENTOR_1;
+import static com.koddy.server.common.utils.TokenUtils.REFRESH_TOKEN;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Auth -> 도메인 [Token] 테스트")
+class TokenTest extends ParallelTest {
+    private final Member member = MENTOR_1.toDomain().apply(1L);
+
+    @Test
+    @DisplayName("Token을 업데이트한다")
+    void updateRefreshToken() {
+        // given
+        final Token token = new Token(member.getId(), REFRESH_TOKEN);
+
+        // when
+        token.updateRefreshToken(REFRESH_TOKEN + "_update");
+
+        // then
+        assertThat(token.getRefreshToken()).isEqualTo(REFRESH_TOKEN + "_update");
+    }
+}
