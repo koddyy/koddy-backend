d4cd3901a63d310e146bf8f83988624655e59389
SeoJiWon <sjiwon4491@gmail.com>
2023-12-28 15:29:29 +0900

    rename: ServerApplication -> KoddyApplication

--- src/main/java/com/koddy/server/KoddyApplication.java
+++ src/main/java/com/koddy/server/KoddyApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerApplication {
+public class KoddyApplication {
     public static void main(final String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
+        SpringApplication.run(KoddyApplication.class, args);
     }
 }
