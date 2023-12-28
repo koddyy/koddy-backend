d3f9b05c593d5d4d610401f8af1d023fbcd201c9
SeoJiWon <sjiwon4491@gmail.com>
2023-12-28 15:52:46 +0900

    test: Flyway + TestContainers를 통한 Repository 테스트 레이어

--- src/test/java/com/koddy/server/common/RepositoryTest.java
+++ src/test/java/com/koddy/server/common/RepositoryTest.java
@@ -1,11 +1,14 @@
 package com.koddy.server.common;
 
+import com.koddy.server.common.config.MySqlTestContainersExtension;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @Tag("Repository")
 @DataJpaTest(showSql = false)
+@ExtendWith(MySqlTestContainersExtension.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public abstract class RepositoryTest {
 }
