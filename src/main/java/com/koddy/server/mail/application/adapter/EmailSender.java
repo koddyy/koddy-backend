3c9e1155bac20352aaa397818587c15175b0bb1f
SeoJiWon <sjiwon4491@gmail.com>
2023-12-29 14:52:43 +0900

    feat: JavaMailSender 기반 이메일 인증번호 메일 전송 로직 구현

--- src/main/java/com/koddy/server/mail/application/adapter/EmailSender.java
+++ src/main/java/com/koddy/server/mail/application/adapter/EmailSender.java
@@ -1,5 +1,5 @@
 package com.koddy.server.mail.application.adapter;
 
 public interface EmailSender {
-    void sendEmailAuthMail(final String email, final String authCode);
+    void sendEmailAuthMail(final String targetEmail, final String authCode);
 }
