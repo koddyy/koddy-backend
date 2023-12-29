7c8120d184cbfd2bce3f31f0a56f8075284c63a3
SeoJiWon <sjiwon4491@gmail.com>
2023-12-29 17:57:08 +0900

    feat: POJO File 도메인(RawFileData) 구현

--- src/main/java/com/koddy/server/file/exception/FileExceptionCode.java
+++ src/main/java/com/koddy/server/file/exception/FileExceptionCode.java
@@ -5,9 +5,12 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
 @Getter
 @RequiredArgsConstructor
 public enum FileExceptionCode implements KoddyExceptionCode {
+    INVALID_FILE_EXTENSION(BAD_REQUEST, "UPLOAD_001", "파일 확장자는 [JPG, JPEG, PNG]만 가능합니다"),
     ;
 
     private final HttpStatus status;
