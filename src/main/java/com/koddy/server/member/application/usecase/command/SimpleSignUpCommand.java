6281ff9e7687a919b90b8359d1a6a455d53fe4ba
SeoJiWon <sjiwon4491@gmail.com>
2023-12-29 12:01:59 +0900

    feat: add MemberType (Mentor, Mentee)

--- src/main/java/com/koddy/server/member/application/usecase/command/SimpleSignUpCommand.java
+++ src/main/java/com/koddy/server/member/application/usecase/command/SimpleSignUpCommand.java
@@ -1,10 +1,12 @@
 package com.koddy.server.member.application.usecase.command;
 
 import com.koddy.server.member.domain.model.Email;
+import com.koddy.server.member.domain.model.MemberType;
 import com.koddy.server.member.domain.model.Password;
 
 public record SimpleSignUpCommand(
         Email email,
-        Password password
+        Password password,
+        MemberType type
 ) {
 }
