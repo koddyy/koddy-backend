de8f400e454234ebf9bd0f15ea35accac84b2238
SeoJiWon <sjiwon4491@gmail.com>
2023-12-30 18:33:00 +0900

    remove: Complete 요청 Record 주석 제거

--- src/main/java/com/koddy/server/member/presentation/dto/request/CompleteMenteeRequest.java
+++ src/main/java/com/koddy/server/member/presentation/dto/request/CompleteMenteeRequest.java
@@ -27,27 +27,3 @@ public record CompleteMenteeRequest(
         String interestMajor
 ) {
 }
-
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@ToString
-//public class CompleteMenteeRequest {
-//    @NotBlank(message = "이름은 필수입니다.")
-//    String name,
-//
-//    @NotNull(message = "국적은 필수입니다.")
-//    Nationality nationality,
-//
-//    String profileUploadUrl,
-//
-//    @NotEmpty(message = "사용 가능한 언어는 하나 이상 선택해야 합니다.")
-//    List<Language> languages,
-//
-//    @NotBlank(message = "관심있는 학교는 필수입니다.")
-//    String interestSchool,
-//
-//    @NotBlank(message = "관심있는 전공은 필수입니다.")
-//    String interestMajor,
-//}
