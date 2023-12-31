80ad7f4f3a6a8edd765258ff74b9a6ed19a18ed4
SeoJiWon <sjiwon4491@gmail.com>
2023-12-31 10:34:56 +0900

    refactor: 이메일 인증코드 확인 후 계정 활성화 적용

--- src/main/java/com/koddy/server/auth/application/usecase/EmailAuthenticationUseCase.java
+++ src/main/java/com/koddy/server/auth/application/usecase/EmailAuthenticationUseCase.java
@@ -4,6 +4,7 @@ import com.koddy.server.auth.application.adapter.MailAuthenticationProcessor;
 import com.koddy.server.auth.application.usecase.command.ConfirmAuthCodeCommand;
 import com.koddy.server.auth.application.usecase.command.SendAuthCodeCommand;
 import com.koddy.server.auth.domain.model.code.AuthKey;
+import com.koddy.server.global.annotation.KoddyWritableTransactional;
 import com.koddy.server.global.annotation.UseCase;
 import com.koddy.server.mail.application.adapter.EmailSender;
 import com.koddy.server.member.domain.model.Member;
@@ -25,12 +26,15 @@ public class EmailAuthenticationUseCase {
         emailSender.sendEmailAuthMail(member.getEmail().getValue(), authCode);
     }
 
+    @KoddyWritableTransactional
     public void confirmAuthCode(final ConfirmAuthCodeCommand command) {
         final Member<?> member = memberRepository.getByEmail(command.email());
 
         final String key = generateAuthKey(member.getEmail().getValue());
         mailAuthenticationProcessor.verifyAuthCode(key, command.authCode());
         mailAuthenticationProcessor.deleteAuthCode(key); // 인증 성공 후 바로 제거 (재활용 X)
+
+        member.authenticate();
     }
 
     private String generateAuthKey(final String email) {
