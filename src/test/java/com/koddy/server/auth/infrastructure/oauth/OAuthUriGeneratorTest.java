444d767c98169605e9a6980dbb4bbc163e4c7137
SeoJiWon <sjiwon4491@gmail.com>
2024-01-01 13:50:19 +0900

    feat: Authorization Code 요청 URL 생성 컴포넌트 구현

---
+++ src/test/java/com/koddy/server/auth/infrastructure/oauth/OAuthUriGeneratorTest.java
@@ -0,0 +1,81 @@
+package com.koddy.server.auth.infrastructure.oauth;
+
+import com.koddy.server.auth.infrastructure.oauth.google.GoogleOAuthProperties;
+import com.koddy.server.auth.infrastructure.oauth.google.GoogleOAuthUriGenerator;
+import com.koddy.server.auth.infrastructure.oauth.kakao.KakaoOAuthProperties;
+import com.koddy.server.auth.infrastructure.oauth.kakao.KakaoOAuthUriGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@EnableConfigurationProperties(value = {
+        GoogleOAuthProperties.class,
+        KakaoOAuthProperties.class
+})
+@SpringBootTest(classes = {
+        GoogleOAuthUriGenerator.class,
+        KakaoOAuthUriGenerator.class
+})
+@DisplayName("Auth -> OAuthUriGenerator [구글, 카카오] 테스트")
+class OAuthUriGeneratorTest {
+    @Autowired
+    private GoogleOAuthUriGenerator googleOAuthUri;
+
+    @Autowired
+    private GoogleOAuthProperties googleOAuthProperties;
+
+    @Autowired
+    private KakaoOAuthUriGenerator kakaoOAuthUri;
+
+    @Autowired
+    private KakaoOAuthProperties kakaoOAuthProperties;
+
+    @Test
+    @DisplayName("Google's Authorization Code를 받기 위한 Authorization Code Request URI를 생성한다")
+    void googleOAuthUri() {
+        // when
+        final String uri = googleOAuthUri.generate(googleOAuthProperties.redirectUri());
+
+        // then
+        final MultiValueMap<String, String> queryParams = UriComponentsBuilder
+                .fromUriString(uri)
+                .build()
+                .getQueryParams();
+
+        assertAll(
+                () -> assertThat(queryParams.getFirst("response_type")).isEqualTo("code"),
+                () -> assertThat(queryParams.getFirst("client_id")).isEqualTo(googleOAuthProperties.clientId()),
+                () -> assertThat(queryParams.getFirst("scope")).isEqualTo(String.join(" ", googleOAuthProperties.scope())),
+                () -> assertThat(queryParams.getFirst("redirect_uri")).isEqualTo(googleOAuthProperties.redirectUri()),
+                () -> assertThat(queryParams.getFirst("state")).isNotNull()
+        );
+    }
+
+    @Test
+    @DisplayName("Kakao's Authorization Code를 받기 위한 Authorization Code Request URI를 생성한다")
+    void kakaoOAuthUri() {
+        // when
+        final String uri = kakaoOAuthUri.generate(kakaoOAuthProperties.redirectUri());
+
+        // then
+        final MultiValueMap<String, String> queryParams = UriComponentsBuilder
+                .fromUriString(uri)
+                .build()
+                .getQueryParams();
+
+        assertAll(
+                () -> assertThat(queryParams.getFirst("response_type")).isEqualTo("code"),
+                () -> assertThat(queryParams.getFirst("client_id")).isEqualTo(kakaoOAuthProperties.clientId()),
+                () -> assertThat(queryParams.getFirst("scope")).isEqualTo(String.join(" ", kakaoOAuthProperties.scope())),
+                () -> assertThat(queryParams.getFirst("redirect_uri")).isEqualTo(kakaoOAuthProperties.redirectUri()),
+                () -> assertThat(queryParams.getFirst("state")).isNotNull()
+        );
+    }
+}
