3aec894d7ab6c133ec03dabe4542b29a4d4557e3
SeoJiWon <sjiwon4491@gmail.com>
2024-01-01 13:34:30 +0900

    feat: 구글, 카카오 OAuth 토큰, 사용자 정보 응답 스펙

---
+++ src/main/java/com/koddy/server/auth/infrastructure/oauth/google/response/GoogleTokenResponse.java
@@ -0,0 +1,15 @@
+package com.koddy.server.auth.infrastructure.oauth.google.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.koddy.server.auth.domain.model.oauth.OAuthTokenResponse;
+
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record GoogleTokenResponse(
+        String tokenType,
+        String idToken,
+        String accessToken,
+        String refreshToken,
+        long expiresIn
+) implements OAuthTokenResponse {
+}
