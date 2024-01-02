5e6b4b7e50b7afe4bc52fa159130f3fa1c6dda38
SeoJiWon <sjiwon4491@gmail.com>
2024-01-02 17:21:49 +0900

    fix: add `@RestController`

--- src/main/java/com/koddy/server/global/HealthCheckController.java
+++ src/main/java/com/koddy/server/global/HealthCheckController.java
@@ -3,8 +3,10 @@ package com.koddy.server.global;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "헬스 체크 API")
+@RestController
 public class HealthCheckController {
     @GetMapping("/api/hello")
     public ResponseEntity<Void> hello() {
