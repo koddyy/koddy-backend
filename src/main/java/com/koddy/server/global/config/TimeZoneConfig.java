247d9ea7a866883d184c4ad992db53dfc5b9b7de
SeoJiWon <sjiwon4491@gmail.com>
2024-01-02 20:11:32 +0900

    chore: 서버, DB 타임존 UTC 설정

---
+++ src/main/java/com/koddy/server/global/config/TimeZoneConfig.java
@@ -0,0 +1,14 @@
+package com.koddy.server.global.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class TimeZoneConfig {
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+}
