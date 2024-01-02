a00c1d72866b7bd5c5d7fffaff0d427b37302e02
SeoJiWon <sjiwon4491@gmail.com>
2024-01-02 17:04:40 +0900

    chore: SwaggerConfig 서버 URL yml 변수로 추출

--- src/main/java/com/koddy/server/global/config/SwaggerConfig.java
+++ src/main/java/com/koddy/server/global/config/SwaggerConfig.java
@@ -2,43 +2,24 @@ package com.koddy.server.global.config;
 
 import com.koddy.server.global.annotation.Auth;
 import com.koddy.server.global.annotation.ExtractToken;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.customizers.OpenApiCustomizer;
-import org.springdoc.core.models.GroupedOpenApi;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.utils.SpringDocUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Koddy API 명세서",
-                description = "Koddy API 명세서",
-                contact = @Contact(
-                        name = "서지원",
-                        url = "https://github.com/koddyy/koddy-backend",
-                        email = "sjiwon4491@gmail.com"
-                ),
-                version = "v1"
-        ),
-        servers = {
-                @Server(
-                        url = "http://localhost:8080",
-                        description = "Local Server"
-                ),
-                @Server(
-                        url = "https://dev:8080",
-                        description = "Dev Server"
-                )
-        }
-)
 public class SwaggerConfig {
     static {
         SpringDocUtils
@@ -49,32 +30,36 @@ public class SwaggerConfig {
                 );
     }
 
-    @Bean
-    public GroupedOpenApi studyWithMeApi() {
-        return GroupedOpenApi.builder()
-                .group("Koddy API 명세서")
-                .pathsToMatch(
-                        "/api/**"
-                )
-                .addOpenApiCustomizer(buildSecurityOpenApi())
-                .build();
-    }
+    @Value("${springdoc.server.url}")
+    private String url;
 
-    private OpenApiCustomizer buildSecurityOpenApi() {
-        return openApi -> openApi
-                .addSecurityItem(
-                        new SecurityRequirement()
-                                .addList("JWT Token")
-                )
-                .getComponents()
-                .addSecuritySchemes(
-                        "JWT Token",
-                        new SecurityScheme()
-                                .name(AUTHORIZATION)
-                                .type(SecurityScheme.Type.HTTP)
-                                .in(SecurityScheme.In.HEADER)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")
-                );
+    @Bean
+    @Profile("!prod")
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes(
+                                "JWT Token",
+                                new SecurityScheme()
+                                        .name(AUTHORIZATION)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ))
+                .security(List.of(
+                        new SecurityRequirement().addList("JWT Token")
+                ))
+                .info(new Info()
+                        .title("Koddy API 명세서")
+                        .description("Koddy API 명세서")
+                        .contact(new Contact()
+                                .name("서지원")
+                                .email("sjiwon4491@gmail.com")
+                                .url("https://github.com/koddyy/koddy-backend"))
+                        .version("v1"))
+                .servers(List.of(
+                        new Server().url(url)
+                ));
     }
 }
