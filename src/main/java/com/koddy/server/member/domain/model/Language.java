03d3af1048450c2248c527e3f5ab908d7ea2fa84
SeoJiWon <sjiwon4491@gmail.com>
2024-01-07 20:52:56 +0900

    refactor: Language에 대한 Category, Type 분류 적용

--- src/main/java/com/koddy/server/member/domain/model/Language.java
+++ src/main/java/com/koddy/server/member/domain/model/Language.java
@@ -1,37 +1,79 @@
 package com.koddy.server.member.domain.model;
 
 import com.koddy.server.member.exception.MemberException;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Enumerated;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static com.koddy.server.member.exception.MemberExceptionCode.INVALID_LANGUAGE;
+import static com.koddy.server.member.exception.MemberExceptionCode.INVALID_LANGUAGE_CATEGORY;
+import static com.koddy.server.member.exception.MemberExceptionCode.INVALID_LANGUAGE_TYPE;
+import static jakarta.persistence.EnumType.STRING;
+import static lombok.AccessLevel.PROTECTED;
 
 @Getter
-@RequiredArgsConstructor
-public enum Language {
-    KR("KR", "한국어"),
-    EN("EN", "영어"),
-    CN("CN", "중국어"),
-    JP("JP", "일본어"),
-    VN("VN", "베트남어"),
-    ;
+@NoArgsConstructor(access = PROTECTED)
+@Embeddable
+public class Language {
+    @Enumerated(STRING)
+    @Column(name = "language_category", nullable = false, columnDefinition = "VARCHAR(20)")
+    private Category category;
 
-    private final String code;
-    private final String value;
+    @Enumerated(STRING)
+    @Column(name = "language_type", nullable = false, columnDefinition = "VARCHAR(20)")
+    private Type type;
 
-    public static Language from(final String code) {
-        return Arrays.stream(values())
-                .filter(it -> it.code.equals(code))
-                .findFirst()
-                .orElseThrow(() -> new MemberException(INVALID_LANGUAGE));
+    public Language(final Category category, final Type type) {
+        this.category = category;
+        this.type = type;
     }
 
-    public static List<Language> of(final List<String> codes) {
-        return codes.stream()
-                .map(Language::from)
-                .toList();
+    @Getter
+    @RequiredArgsConstructor
+    public enum Category {
+        KR("KR", "한국어"),
+        EN("EN", "영어"),
+        CN("CN", "중국어"),
+        JP("JP", "일본어"),
+        VN("VN", "베트남어"),
+        ;
+
+        private final String code;
+        private final String value;
+
+        public static Category from(final String code) {
+            return Arrays.stream(values())
+                    .filter(it -> it.code.equals(code))
+                    .findFirst()
+                    .orElseThrow(() -> new MemberException(INVALID_LANGUAGE_CATEGORY));
+        }
+
+        public static List<Category> of(final List<String> codes) {
+            return codes.stream()
+                    .map(Category::from)
+                    .toList();
+        }
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum Type {
+        MAIN("메인 언어"),
+        SUB("서브 언어"),
+        ;
+
+        private final String value;
+
+        public static Type from(final String value) {
+            return Arrays.stream(values())
+                    .filter(it -> it.value.equals(value))
+                    .findFirst()
+                    .orElseThrow(() -> new MemberException(INVALID_LANGUAGE_TYPE));
+        }
     }
 }
