3d07e9e110fb517844c2d2c7c728db31996ff2d7
SeoJiWon <sjiwon4491@gmail.com>
2024-01-10 00:23:29 +0900

    refactor: 커스텀 RedisOperator 인터페이스 추가

--- src/main/java/com/koddy/server/global/redis/StringRedisOperator.java
+++ src/main/java/com/koddy/server/global/redis/StringRedisOperator.java
@@ -1,4 +1,4 @@
-package com.koddy.server.global.utils;
+package com.koddy.server.global.redis;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -8,21 +8,25 @@ import java.time.Duration;
 
 @Component
 @RequiredArgsConstructor
-public class DefaultRedisStringOperator {
+public class StringRedisOperator implements RedisOperator<String, String> {
     private final StringRedisTemplate redisTemplate;
 
+    @Override
     public void save(final String key, final String value, final Duration duration) {
         redisTemplate.opsForValue().set(key, value, duration);
     }
 
+    @Override
     public String get(final String key) {
         return redisTemplate.opsForValue().get(key);
     }
 
+    @Override
     public boolean contains(final String key) {
         return redisTemplate.opsForValue().get(key) != null;
     }
 
+    @Override
     public void delete(final String key) {
         redisTemplate.delete(key);
     }
