06f5cefb7aff33a9d35bdb9e0f6048ee44d0da95
SeoJiWon <sjiwon4491@gmail.com>
2024-01-11 18:33:22 +0900

    feat: OAuth 인증, 로그아웃, 토큰 재발급 관련 인수테스트 작성

---
+++ src/test/java/com/koddy/server/acceptance/auth/AuthAcceptanceStep.java
@@ -0,0 +1,60 @@
+package com.koddy.server.acceptance.auth;
+
+import com.koddy.server.auth.presentation.dto.request.OAuthLoginRequest;
+import io.restassured.response.ValidatableResponse;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import static com.koddy.server.acceptance.CommonRequestFixture.getRequest;
+import static com.koddy.server.acceptance.CommonRequestFixture.postRequest;
+import static com.koddy.server.acceptance.CommonRequestFixture.postRequestWithAccessToken;
+import static com.koddy.server.acceptance.CommonRequestFixture.postRequestWithRefreshToken;
+
+public class AuthAcceptanceStep {
+    public static ValidatableResponse Google_OAuth_인증_URL를_생성한다(
+            final String oAuthProvider,
+            final String redirectUri
+    ) {
+        final String uri = UriComponentsBuilder
+                .fromPath("/api/oauth/access/{provider}?redirectUri={redirectUri}")
+                .build(oAuthProvider, redirectUri)
+                .getPath();
+
+        return getRequest(uri);
+    }
+
+    public static ValidatableResponse Google_OAuth_로그인을_진행한다(
+            final String oAuthProvider,
+            final String authorizationCode,
+            final String redirectUri,
+            final String state
+    ) {
+        final String uri = UriComponentsBuilder
+                .fromPath("/api/oauth/login/{provider}")
+                .build(oAuthProvider)
+                .getPath();
+
+        final OAuthLoginRequest request = new OAuthLoginRequest(authorizationCode, redirectUri, state);
+
+        return postRequest(request, uri);
+    }
+
+    public static ValidatableResponse 로그아웃을_진행한다(final String accessToken) {
+        final String uri = UriComponentsBuilder
+                .fromPath("/api/oauth/logout")
+                .build()
+                .toUri()
+                .getPath();
+
+        return postRequestWithAccessToken(accessToken, uri);
+    }
+
+    public static ValidatableResponse 토큰을_재발급받는다(final String refreshToken) {
+        final String uri = UriComponentsBuilder
+                .fromPath("/api/token/reissue")
+                .build()
+                .toUri()
+                .getPath();
+
+        return postRequestWithRefreshToken(refreshToken, uri);
+    }
+}
---
+++ src/test/java/com/koddy/server/acceptance/auth/TokenReissueAcceptanceTest.java
@@ -0,0 +1,35 @@
+package com.koddy.server.acceptance.auth;
+
+import com.koddy.server.common.AcceptanceTest;
+import com.koddy.server.common.config.DatabaseCleanerEachCallbackExtension;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static com.koddy.server.acceptance.auth.AuthAcceptanceStep.토큰을_재발급받는다;
+import static com.koddy.server.auth.utils.TokenResponseWriter.COOKIE_REFRESH_TOKEN;
+import static com.koddy.server.common.fixture.MentorFixture.MENTOR_1;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.SET_COOKIE;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+
+@ExtendWith(DatabaseCleanerEachCallbackExtension.class)
+@DisplayName("[Acceptance Test] 토큰 재발급")
+public class TokenReissueAcceptanceTest extends AcceptanceTest {
+    @Nested
+    @DisplayName("토큰 재발급 API")
+    class TokenReissueApi {
+        @Test
+        @DisplayName("유효한 RefreshToken을 통해서 AccessToken + RefreshToken을 재발급받는다")
+        void success() {
+            final String refreshToken = MENTOR_1.회원가입_로그인_후_RefreshToken을_추출한다();
+            토큰을_재발급받는다(refreshToken)
+                    .statusCode(NO_CONTENT.value())
+                    .header(AUTHORIZATION, notNullValue(String.class))
+                    .header(SET_COOKIE, notNullValue(String.class))
+                    .cookie(COOKIE_REFRESH_TOKEN, notNullValue(String.class));
+        }
+    }
+}
