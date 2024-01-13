efbbbc3d5c9e238808ea72eaed3f074b3fc19885
SeoJiWon <sjiwon4491@gmail.com>
2024-01-13 21:27:50 +0900

    feat: MDC 기반 로깅 메타정보 추적 로직

---
+++ src/main/java/com/koddy/server/global/filter/MdcKey.java
@@ -0,0 +1,11 @@
+package com.koddy.server.global.filter;
+
+public enum MdcKey {
+    REQUEST_ID,
+    REQUEST_IP,
+    REQUEST_METHOD,
+    REQUEST_URI,
+    REQUEST_PARAMS,
+    REQUEST_TIME,
+    START_TIME_MILLIS,
+}
