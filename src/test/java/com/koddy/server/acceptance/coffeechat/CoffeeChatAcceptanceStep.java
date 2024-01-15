cd2d6edfa5eec06c19c799f63b3962a3e3ad87fa
SeoJiWon <sjiwon4491@gmail.com>
2024-01-15 21:31:08 +0900

    test: Pending 상태인 커피챗에 대한 최종 처리 관련 인수테스트 작성

--- src/test/java/com/koddy/server/acceptance/coffeechat/CoffeeChatAcceptanceStep.java
+++ src/test/java/com/koddy/server/acceptance/coffeechat/CoffeeChatAcceptanceStep.java
@@ -1,11 +1,13 @@
 package com.koddy.server.acceptance.coffeechat;
 
 import com.koddy.server.coffeechat.presentation.dto.request.ApproveAppliedCoffeeChatRequest;
+import com.koddy.server.coffeechat.presentation.dto.request.ApprovePendingCoffeeChatRequest;
 import com.koddy.server.coffeechat.presentation.dto.request.CreateMeetingLinkRequest;
 import com.koddy.server.coffeechat.presentation.dto.request.MenteeApplyCoffeeChatRequest;
 import com.koddy.server.coffeechat.presentation.dto.request.MentorSuggestCoffeeChatRequest;
 import com.koddy.server.coffeechat.presentation.dto.request.PendingSuggestedCoffeeChatRequest;
 import com.koddy.server.coffeechat.presentation.dto.request.RejectAppliedCoffeeChatRequest;
+import com.koddy.server.coffeechat.presentation.dto.request.RejectPendingCoffeeChatRequest;
 import com.koddy.server.coffeechat.presentation.dto.request.RejectSuggestedCoffeeChatRequest;
 import com.koddy.server.common.fixture.StrategyFixture;
 import io.restassured.response.ValidatableResponse;
@@ -175,6 +177,36 @@ public class CoffeeChatAcceptanceStep {
                 .getPath();
 
         final PendingSuggestedCoffeeChatRequest request = new PendingSuggestedCoffeeChatRequest(start, end);
+
+        return patchRequestWithAccessToken(uri, request, accessToken);
+    }
+
+    public static ValidatableResponse 멘토가_Pending_상태인_커피챗에_대해서_최종_거절을_한다(
+            final long coffeeChatId,
+            final String rejectReason,
+            final String accessToken
+    ) {
+        final String uri = UriComponentsBuilder
+                .fromPath("/api/coffeechats/pending/reject/{coffeeChatId}")
+                .build(coffeeChatId)
+                .getPath();
+
+        final RejectPendingCoffeeChatRequest request = new RejectPendingCoffeeChatRequest(rejectReason);
+
+        return patchRequestWithAccessToken(uri, request, accessToken);
+    }
+
+    public static ValidatableResponse 멘토가_Pending_상태인_커피챗에_대해서_최종_수락을_한다(
+            final long coffeeChatId,
+            final StrategyFixture fixture,
+            final String accessToken
+    ) {
+        final String uri = UriComponentsBuilder
+                .fromPath("/api/coffeechats/pending/approve/{coffeeChatId}")
+                .build(coffeeChatId)
+                .getPath();
+
+        final ApprovePendingCoffeeChatRequest request = new ApprovePendingCoffeeChatRequest(fixture.getType().getEng(), fixture.getValue());
 
         return patchRequestWithAccessToken(uri, request, accessToken);
     }
