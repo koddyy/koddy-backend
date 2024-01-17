8e49392c397bf298c4704b1175a3e34555bf8fb9
SeoJiWon <sjiwon4491@gmail.com>
2024-01-17 16:20:20 +0900

    feat: RDB 전용 토큰 저장소

--- src/main/java/com/koddy/server/auth/infrastructure/token/RdbTokenStore.java
+++ src/main/java/com/koddy/server/auth/infrastructure/token/RdbTokenStore.java
@@ -1,18 +1,20 @@
-package com.koddy.server.auth.domain.service;
+package com.koddy.server.auth.infrastructure.token;
 
+import com.koddy.server.auth.application.adapter.TokenStore;
 import com.koddy.server.auth.domain.model.Token;
 import com.koddy.server.auth.domain.repository.TokenRepository;
 import com.koddy.server.global.annotation.KoddyWritableTransactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 @RequiredArgsConstructor
-public class TokenManager {
+public class RdbTokenStore implements TokenStore {
     private final TokenRepository tokenRepository;
 
     @KoddyWritableTransactional
-    public void synchronizeRefreshToken(final long memberId, final String refreshToken) {
+    @Override
+    public void synchronizeRefreshToken(final Long memberId, final String refreshToken) {
         tokenRepository.findByMemberId(memberId)
                 .ifPresentOrElse(
                         it -> it.updateRefreshToken(refreshToken),
@@ -20,14 +22,17 @@ public class TokenManager {
                 );
     }
 
+    @Override
     public void updateRefreshToken(final long memberId, final String newRefreshToken) {
         tokenRepository.updateRefreshToken(memberId, newRefreshToken);
     }
 
+    @Override
     public void deleteRefreshToken(final long memberId) {
         tokenRepository.deleteRefreshToken(memberId);
     }
 
+    @Override
     public boolean isMemberRefreshToken(final long memberId, final String refreshToken) {
         return tokenRepository.existsByMemberIdAndRefreshToken(memberId, refreshToken);
     }
