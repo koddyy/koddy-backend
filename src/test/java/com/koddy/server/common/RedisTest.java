ed7d45955d75252443d89a519e4c45521e02cda2
sjiwon <sjiwon4491@gmail.com>
2024-01-17 16:52:54 +0900

    [Refactor] 토큰 저장소를 Redis로 교체한다 (#93)
    * feat: 토큰 저장소 인터페이스
    
    * feat: RDB 전용 토큰 저장소
    
    * feat: Redis 전용 토큰 저장소
    
    * test: RedisTokenStore 테스트 코드 작성
    
    * refactor: TokenIssuer 의존 TokenManager -> TokenStore

---
+++ src/test/java/com/koddy/server/common/RedisTest.java
@@ -0,0 +1,12 @@
+package com.koddy.server.common;
+
+import com.koddy.server.common.containers.RedisTestContainersExtension;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
+
+@Tag("Redis")
+@DataRedisTest
+@ExtendWith(RedisTestContainersExtension.class)
+public abstract class RedisTest {
+}
