26c71aec7f5165dbc2f0e077f899f34924b154d8
SeoJiWon <sjiwon4491@gmail.com>
2024-01-21 14:49:33 +0900

    feat: 특정 Year-Month에 멘토의 예약된 시간 조회 쿼리

--- src/main/java/com/koddy/server/coffeechat/domain/repository/CoffeeChatRepository.java
+++ src/main/java/com/koddy/server/coffeechat/domain/repository/CoffeeChatRepository.java
@@ -5,9 +5,13 @@ import com.koddy.server.coffeechat.domain.model.CoffeeChatStatus;
 import com.koddy.server.coffeechat.exception.CoffeeChatException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static com.koddy.server.coffeechat.domain.model.CoffeeChatStatus.APPLY;
+import static com.koddy.server.coffeechat.domain.model.CoffeeChatStatus.APPROVE;
 import static com.koddy.server.coffeechat.domain.model.CoffeeChatStatus.PENDING;
 import static com.koddy.server.coffeechat.exception.CoffeeChatExceptionCode.APPLIED_COFFEE_CHAT_NOT_FOUND;
 import static com.koddy.server.coffeechat.exception.CoffeeChatExceptionCode.APPLIED_OR_SUGGESTED_COFFEE_CHAT_NOT_FOUND;
@@ -31,5 +35,30 @@ public interface CoffeeChatRepository extends JpaRepository<CoffeeChat, Long> {
     default CoffeeChat getAppliedOrSuggestedCoffeeChat(final Long id, final Long sourceMemberId) {
         return findByIdAndSourceMemberIdAndStatus(id, sourceMemberId, APPLY)
                 .orElseThrow(() -> new CoffeeChatException(APPLIED_OR_SUGGESTED_COFFEE_CHAT_NOT_FOUND));
+    }
+
+    List<CoffeeChat> findBySourceMemberIdAndStartYearAndStartMonthAndStatusIn(
+            final long sourceMemberId,
+            final int year,
+            final int month,
+            final List<CoffeeChatStatus> status
+    );
+
+    List<CoffeeChat> findByTargetMemberIdAndStartYearAndStartMonthAndStatusIn(
+            final long targetMemberId,
+            final int year,
+            final int month,
+            final List<CoffeeChatStatus> status
+    );
+
+    default List<CoffeeChat> getReservedCoffeeChat(final long mentorId, final int year, final int month) {
+        final List<CoffeeChat> suggestedByMentor =
+                findBySourceMemberIdAndStartYearAndStartMonthAndStatusIn(mentorId, year, month, List.of(PENDING, APPROVE));
+        final List<CoffeeChat> appliedToMentor =
+                findByTargetMemberIdAndStartYearAndStartMonthAndStatusIn(mentorId, year, month, List.of(APPLY, APPROVE));
+
+        return Stream.concat(suggestedByMentor.stream(), appliedToMentor.stream())
+                .sorted(Comparator.comparing(it -> it.getStart().toLocalDateTime()))
+                .toList();
     }
 }
