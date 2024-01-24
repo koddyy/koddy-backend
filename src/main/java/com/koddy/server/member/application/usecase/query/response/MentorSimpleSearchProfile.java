95578156b2a42c96c9b58a20699749e862e0596c
sjiwon <sjiwon4491@gmail.com>
2024-01-25 00:43:15 +0900

    feat: 멘티 메인 홈 커피챗 제안한 멘토, 멘토 둘러보기 Query/UseCase 구현

---
+++ src/main/java/com/koddy/server/member/application/usecase/query/response/MentorSimpleSearchProfile.java
@@ -0,0 +1,23 @@
+package com.koddy.server.member.application.usecase.query.response;
+
+import com.koddy.server.member.domain.model.mentor.Mentor;
+
+public record MentorSimpleSearchProfile(
+        long id,
+        String name,
+        String profileImageUrl,
+        String school,
+        String major,
+        int enteredIn
+) {
+    public static MentorSimpleSearchProfile of(final Mentor mentor) {
+        return new MentorSimpleSearchProfile(
+                mentor.getId(),
+                mentor.getName(),
+                mentor.getProfileImageUrl(),
+                mentor.getUniversityProfile().getSchool(),
+                mentor.getUniversityProfile().getMajor(),
+                mentor.getUniversityProfile().getEnteredIn()
+        );
+    }
+}
