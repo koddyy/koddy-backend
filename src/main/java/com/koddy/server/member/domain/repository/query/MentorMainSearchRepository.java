c0e9cbcc55f0f22f8ec9dccf6c36c5a1058ff0b4
sjiwon <sjiwon4491@gmail.com>
2024-01-24 17:10:55 +0900

    refactor: 신청한 멘티 쿼리에 mentor 자신 ID 필터링 추가

--- src/main/java/com/koddy/server/member/domain/repository/query/MentorMainSearchRepository.java
+++ src/main/java/com/koddy/server/member/domain/repository/query/MentorMainSearchRepository.java
@@ -2,12 +2,13 @@ package com.koddy.server.member.domain.repository.query;
 
 import com.koddy.server.member.domain.model.mentee.Mentee;
 import com.koddy.server.member.domain.repository.query.spec.SearchMentee;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
 import java.util.List;
 
 public interface MentorMainSearchRepository {
-    List<Mentee> findAppliedMentees(final int limit);
+    List<Mentee> findAppliedMentees(final long mentorId, final int limit);
 
-    Slice<Mentee> findMentees(final SearchMentee search);
+    Slice<Mentee> findMentees(final SearchMentee search, final Pageable pageable);
 }
