415b0b956c673a6791d2ddee59d45ad36d227cd3
sjiwon <sjiwon4491@gmail.com>
2024-01-24 16:41:09 +0900

    refactor: 둘러보기 필터링 가능한 조합별 스펙 수정

--- src/main/java/com/koddy/server/member/domain/repository/query/spec/SearchMentee.java
+++ src/main/java/com/koddy/server/member/domain/repository/query/spec/SearchMentee.java
@@ -3,18 +3,36 @@ package com.koddy.server.member.domain.repository.query.spec;
 import com.koddy.server.global.exception.GlobalException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.util.CollectionUtils;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import static com.koddy.server.global.exception.GlobalExceptionCode.INVALID_SORT_TYPE;
-import static com.koddy.server.member.domain.repository.query.spec.SearchMentee.Sort.LATEST;
 
 public record SearchMentee(
-        Sort sort,
-        List<String> values
+        LatestCondition latest,
+        NationalityCondition nationality,
+        LanguageCondition language
 ) {
+    public record LatestCondition(
+            boolean contains
+    ) {
+    }
+
+    public record NationalityCondition(
+            boolean contains,
+            List<String> values
+    ) {
+    }
+
+    public record LanguageCondition(
+            boolean contains,
+            List<String> values
+    ) {
+    }
+
     @Getter
     @RequiredArgsConstructor
     public enum Sort {
@@ -34,10 +52,40 @@ public record SearchMentee(
     }
 
     public static SearchMentee basic() {
-        return new SearchMentee(LATEST, Collections.emptyList());
+        return new SearchMentee(
+                createLatestCondition(true),
+                createNationalityCondition(Collections.emptyList()),
+                createLanguageCondition(Collections.emptyList())
+        );
     }
 
-    public static SearchMentee of(final Sort sort, final List<String> values) {
-        return new SearchMentee(sort, values);
+    public static SearchMentee of(
+            final boolean containsLatest,
+            final List<String> nationalities,
+            final List<String> languages
+    ) {
+        return new SearchMentee(
+                createLatestCondition(containsLatest),
+                createNationalityCondition(nationalities),
+                createLanguageCondition(languages)
+        );
+    }
+
+    private static LatestCondition createLatestCondition(final boolean containsLatest) {
+        return new LatestCondition(containsLatest);
+    }
+
+    private static NationalityCondition createNationalityCondition(final List<String> nationalities) {
+        if (CollectionUtils.isEmpty(nationalities)) {
+            return new NationalityCondition(false, Collections.emptyList());
+        }
+        return new NationalityCondition(true, nationalities);
+    }
+
+    private static LanguageCondition createLanguageCondition(final List<String> languages) {
+        if (CollectionUtils.isEmpty(languages)) {
+            return new LanguageCondition(false, Collections.emptyList());
+        }
+        return new LanguageCondition(true, languages);
     }
 }
