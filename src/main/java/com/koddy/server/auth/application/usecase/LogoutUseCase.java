9ade190dc6d98584493ea0702c6dff7d4c88bd63
sjiwon <sjiwon4491@gmail.com>
2024-02-02 14:58:26 +0900

    refactor: LogoutUseCase 사용자 존재유무 확인

--- src/main/java/com/koddy/server/auth/application/usecase/LogoutUseCase.java
+++ src/main/java/com/koddy/server/auth/application/usecase/LogoutUseCase.java
@@ -3,14 +3,18 @@ package com.koddy.server.auth.application.usecase;
 import com.koddy.server.auth.application.usecase.command.LogoutCommand;
 import com.koddy.server.auth.domain.service.TokenIssuer;
 import com.koddy.server.global.annotation.UseCase;
+import com.koddy.server.member.domain.model.Member;
+import com.koddy.server.member.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 
 @UseCase
 @RequiredArgsConstructor
 public class LogoutUseCase {
+    private final MemberRepository memberRepository;
     private final TokenIssuer tokenIssuer;
 
     public void invoke(final LogoutCommand command) {
-        tokenIssuer.deleteRefreshToken(command.memberId());
+        final Member<?> member = memberRepository.getById(command.memberId());
+        tokenIssuer.deleteRefreshToken(member.getId());
     }
 }
