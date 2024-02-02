4a77e9505791ec03506fa47dca46fa361052121f
sjiwon <sjiwon4491@gmail.com>
2024-02-02 17:39:23 +0900

    refactor: 사용자 탈퇴 UseCase 로직 수정

--- src/test/java/com/koddy/server/member/application/usecase/DeleteMemberUseCaseTest.java
+++ src/test/java/com/koddy/server/member/application/usecase/DeleteMemberUseCaseTest.java
@@ -1,16 +1,16 @@
 package com.koddy.server.member.application.usecase;
 
 import com.koddy.server.common.UnitTest;
-import com.koddy.server.member.application.usecase.command.DeleteMemberCommand;
+import com.koddy.server.member.domain.model.mentee.Mentee;
+import com.koddy.server.member.domain.model.mentor.Mentor;
 import com.koddy.server.member.domain.repository.MemberRepository;
 import com.koddy.server.member.domain.service.MenteeDeleter;
 import com.koddy.server.member.domain.service.MentorDeleter;
-import com.koddy.server.member.exception.MemberException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.koddy.server.member.exception.MemberExceptionCode.MEMBER_NOT_FOUND;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static com.koddy.server.common.fixture.MenteeFixture.MENTEE_1;
+import static com.koddy.server.common.fixture.MentorFixture.MENTOR_1;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
@@ -28,62 +28,40 @@ class DeleteMemberUseCaseTest extends UnitTest {
             menteeDeleter
     );
 
-    private final DeleteMemberCommand command = new DeleteMemberCommand(1);
+    private final Mentor mentor = MENTOR_1.toDomain().apply(1L);
+    private final Mentee mentee = MENTEE_1.toDomain().apply(2L);
 
     @Test
-    @DisplayName("존재하지 않는 사용자는 삭제할 수 없다")
-    void throwExceptionByMemberNotFound() {
-        // given
-        given(memberRepository.existsById(command.memberId())).willReturn(false);
-
-        // when - then
-        assertThatThrownBy(() -> sut.invoke(command))
-                .isInstanceOf(MemberException.class)
-                .hasMessage(MEMBER_NOT_FOUND.getMessage());
-
-        assertAll(
-                () -> verify(memberRepository, times(1)).existsById(command.memberId()),
-                () -> verify(memberRepository, times(0)).isMentor(command.memberId()),
-                () -> verify(mentorDeleter, times(0)).execute(command.memberId()),
-                () -> verify(menteeDeleter, times(0)).execute(command.memberId())
-        );
-    }
-
-    @Test
-    @DisplayName("Mentor를 삭제한다")
+    @DisplayName("멘토를 삭제한다")
     void deleteMentor() {
         // given
-        given(memberRepository.existsById(command.memberId())).willReturn(true);
-        given(memberRepository.isMentor(command.memberId())).willReturn(true);
+        given(memberRepository.getById(mentor.getId())).willReturn(mentor);
 
         // when
-        sut.invoke(command);
+        sut.invoke(mentor.getId());
 
         // then
         assertAll(
-                () -> verify(memberRepository, times(1)).existsById(command.memberId()),
-                () -> verify(memberRepository, times(1)).isMentor(command.memberId()),
-                () -> verify(mentorDeleter, times(1)).execute(command.memberId()),
-                () -> verify(menteeDeleter, times(0)).execute(command.memberId())
+                () -> verify(memberRepository, times(1)).getById(mentor.getId()),
+                () -> verify(mentorDeleter, times(1)).execute(mentor.getId()),
+                () -> verify(menteeDeleter, times(0)).execute(mentor.getId())
         );
     }
 
     @Test
-    @DisplayName("Mentee를 삭제한다")
+    @DisplayName("멘티를 삭제한다")
     void deleteMentee() {
         // given
-        given(memberRepository.existsById(command.memberId())).willReturn(true);
-        given(memberRepository.isMentor(command.memberId())).willReturn(false);
+        given(memberRepository.getById(mentee.getId())).willReturn(mentee);
 
         // when
-        sut.invoke(command);
+        sut.invoke(mentee.getId());
 
         // then
         assertAll(
-                () -> verify(memberRepository, times(1)).existsById(command.memberId()),
-                () -> verify(memberRepository, times(1)).isMentor(command.memberId()),
-                () -> verify(mentorDeleter, times(0)).execute(command.memberId()),
-                () -> verify(menteeDeleter, times(1)).execute(command.memberId())
+                () -> verify(memberRepository, times(1)).getById(mentee.getId()),
+                () -> verify(mentorDeleter, times(0)).execute(mentee.getId()),
+                () -> verify(menteeDeleter, times(1)).execute(mentee.getId())
         );
     }
 }
