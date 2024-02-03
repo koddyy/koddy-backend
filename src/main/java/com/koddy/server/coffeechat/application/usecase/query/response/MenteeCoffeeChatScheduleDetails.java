5f79a427357dbd9d7f103e067b2bdc09a757c236
sjiwon <sjiwon4491@gmail.com>
2024-02-03 16:26:17 +0900

    feat: 커피챗 디테일 정보 조회 응답 스펙 정의

---
+++ src/main/java/com/koddy/server/coffeechat/application/usecase/query/response/MenteeCoffeeChatScheduleDetails.java
@@ -0,0 +1,24 @@
+package com.koddy.server.coffeechat.application.usecase.query.response;
+
+import com.koddy.server.coffeechat.domain.model.CoffeeChat;
+import com.koddy.server.coffeechat.domain.model.response.CoffeeChatDetails;
+import com.koddy.server.coffeechat.domain.model.response.CoffeeChatScheduleDetails;
+import com.koddy.server.coffeechat.domain.model.response.MentorDetails;
+import com.koddy.server.global.utils.encrypt.Encryptor;
+import com.koddy.server.member.domain.model.mentor.Mentor;
+
+public record MenteeCoffeeChatScheduleDetails(
+        MentorDetails mentor,
+        CoffeeChatDetails coffeeChat
+) implements CoffeeChatScheduleDetails {
+    public static MenteeCoffeeChatScheduleDetails of(
+            final Mentor mentor,
+            final CoffeeChat coffeeChat,
+            final Encryptor encryptor
+    ) {
+        return new MenteeCoffeeChatScheduleDetails(
+                MentorDetails.of(mentor),
+                CoffeeChatDetails.of(coffeeChat, encryptor)
+        );
+    }
+}
---
+++ src/main/java/com/koddy/server/coffeechat/domain/model/response/MentorDetails.java
@@ -0,0 +1,28 @@
+package com.koddy.server.coffeechat.domain.model.response;
+
+import com.koddy.server.member.domain.model.mentor.Mentor;
+import com.koddy.server.member.domain.model.response.LanguageResponse;
+
+public record MentorDetails(
+        long id,
+        String name,
+        String profileImageUrl,
+        String introduction,
+        LanguageResponse languages,
+        String school,
+        String major,
+        int enteredIn
+) {
+    public static MentorDetails of(final Mentor mentor) {
+        return new MentorDetails(
+                mentor.getId(),
+                mentor.getName(),
+                mentor.getProfileImageUrl(),
+                mentor.getIntroduction(),
+                LanguageResponse.of(mentor.getLanguages()),
+                mentor.getUniversityProfile().getSchool(),
+                mentor.getUniversityProfile().getMajor(),
+                mentor.getUniversityProfile().getEnteredIn()
+        );
+    }
+}
