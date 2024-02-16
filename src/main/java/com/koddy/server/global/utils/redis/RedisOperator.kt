7b220f7412be41bb226a86c6acddb0b94812de7f
sjiwon <sjiwon4491@gmail.com>
2024-02-16 19:17:37 +0900

    refactor: RedisOperator, StringRedisOperator Kotlin 전환

---
+++ src/main/java/com/koddy/server/global/utils/redis/RedisOperator.kt
@@ -0,0 +1,38 @@
+package com.koddy.server.global.utils.redis
+
+import java.time.Duration
+import java.util.concurrent.TimeUnit
+
+interface RedisOperator<K, V> {
+    fun save(
+        key: K,
+        value: V,
+        timeout: Long,
+        timeUnit: TimeUnit,
+    )
+
+    fun save(
+        key: K,
+        value: V,
+        duration: Duration,
+    )
+
+    fun get(key: K): V
+
+    fun contains(key: K): Boolean
+
+    fun delete(key: K)
+
+    fun incr(key: K): Long
+
+    fun incr(
+        key: K,
+        timeout: Long,
+        timeUnit: TimeUnit,
+    ): Long
+
+    fun incr(
+        key: K,
+        duration: Duration,
+    ): Long
+}
