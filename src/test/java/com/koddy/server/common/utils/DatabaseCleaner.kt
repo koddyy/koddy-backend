b5c3272bf23a6a48472c4ff9e00f82b4a24a3877
sjiwon <sjiwon4491@gmail.com>
2024-02-17 00:46:11 +0900

    refactor: DatabaseCleaner Kotlin 전환

---
+++ src/test/java/com/koddy/server/common/utils/DatabaseCleaner.kt
@@ -0,0 +1,30 @@
+package com.koddy.server.common.utils
+
+import jakarta.persistence.EntityManager
+import jakarta.persistence.Table
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+@Component
+class DatabaseCleaner(
+    private val entityManager: EntityManager,
+) {
+    private val tableNames: List<String> =
+        entityManager.metamodel
+            .entities
+            .map { it.javaType }
+            .map { it.getAnnotation(Table::class.java) }
+            .map { it.name }
+
+    @Transactional
+    fun cleanUpDatabase() {
+        entityManager.flush()
+        entityManager.createNativeQuery("SET foreign_key_checks = 0").executeUpdate()
+
+        for (tableName in tableNames) {
+            entityManager.createNativeQuery("TRUNCATE TABLE $tableName").executeUpdate()
+        }
+
+        entityManager.createNativeQuery("SET foreign_key_checks = 1").executeUpdate()
+    }
+}
