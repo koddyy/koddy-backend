e68b91e9f41141a07d1a419ab4bdf77d76683cf3
sjiwon <sjiwon4491@gmail.com>
2024-02-26 21:21:00 +0900

    refactor: 더미 사용자 로그인 로직 api layer에서 처리
    - 개발 서버 일회성이기 때문에 굳이 layer 나눌 필요 X

--- src/main/java/com/koddy/server/admin/controller/DummyAccountLoginApi.kt
+++ src/main/java/com/koddy/server/admin/controller/DummyAccountLoginApi.kt
@@ -1,10 +1,17 @@
 package com.koddy.server.admin.controller
 
-import com.koddy.server.admin.service.DummyAccountLoginService
 import com.koddy.server.admin.utils.NotProvidedInProduction
-import com.koddy.server.auth.domain.model.AuthMember
+import com.koddy.server.auth.domain.model.AuthToken
+import com.koddy.server.auth.domain.service.TokenIssuer
+import com.koddy.server.auth.utils.TokenResponseWriter
 import com.koddy.server.member.domain.model.Email
+import com.koddy.server.member.domain.model.Member
+import com.koddy.server.member.domain.repository.MemberRepository
+import com.koddy.server.member.exception.MemberException
+import com.koddy.server.member.exception.MemberExceptionCode.MEMBER_NOT_FOUND
+import jakarta.servlet.http.HttpServletResponse
 import jakarta.validation.constraints.NotBlank
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -14,7 +21,10 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/auth/dummy/login")
 class DummyAccountLoginApi(
-    private val dummyAccountLoginService: DummyAccountLoginService,
+    private val memberRepository: MemberRepository,
+    private val tokenIssuer: TokenIssuer,
+    private val tokenResponseWriter: TokenResponseWriter,
+    @Value("\${account.dummy}") private val dummyAccountPassword: String,
 ) {
     data class LoginRequest(
         @field:NotBlank(message = "이메일은 필수입니다.")
@@ -28,8 +38,17 @@ class DummyAccountLoginApi(
     @NotProvidedInProduction
     fun login(
         @RequestBody request: LoginRequest,
-    ): ResponseEntity<AuthMember> {
-        val authMember: AuthMember = dummyAccountLoginService.invoke(Email.from(request.email), request.password)
-        return ResponseEntity.ok(authMember)
+        response: HttpServletResponse,
+    ): ResponseEntity<Void> {
+        val member: Member<*> = memberRepository.findByPlatformEmail(Email.from(request.email))
+            .orElseThrow { MemberException(MEMBER_NOT_FOUND) }
+
+        if (dummyAccountPassword != request.password) {
+            throw MemberException(MEMBER_NOT_FOUND)
+        }
+
+        val authToken: AuthToken = tokenIssuer.provideAuthorityToken(member.id, member.authority)
+        tokenResponseWriter.applyToken(response, authToken)
+        return ResponseEntity.ok().build()
     }
 }
