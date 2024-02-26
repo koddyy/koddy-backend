8ba79b276e7364e55c3f0ddc120f72d03494a570
sjiwon <sjiwon4491@gmail.com>
2024-02-26 21:30:13 +0900

    fix: 내 일정 카테고리별 개수 조회 임시 npe 처리

--- src/main/java/com/koddy/server/coffeechat/application/usecase/GetCoffeeChatScheduleUseCase.java
+++ src/main/java/com/koddy/server/coffeechat/application/usecase/GetCoffeeChatScheduleUseCase.java
@@ -4,6 +4,7 @@ import com.koddy.server.auth.domain.model.Authenticated;
 import com.koddy.server.coffeechat.application.usecase.query.GetMenteeCoffeeChats;
 import com.koddy.server.coffeechat.application.usecase.query.GetMentorCoffeeChats;
 import com.koddy.server.coffeechat.application.usecase.query.response.CoffeeChatEachCategoryCounts;
+import com.koddy.server.coffeechat.domain.repository.CoffeeChatRepository;
 import com.koddy.server.coffeechat.domain.repository.query.CoffeeChatScheduleQueryRepository;
 import com.koddy.server.coffeechat.domain.repository.query.response.MenteeCoffeeChatScheduleData;
 import com.koddy.server.coffeechat.domain.repository.query.response.MentorCoffeeChatScheduleData;
@@ -19,17 +20,34 @@ import java.util.List;
 
 @UseCase
 public class GetCoffeeChatScheduleUseCase {
+    private final CoffeeChatRepository coffeeChatRepository;
     private final CoffeeChatScheduleQueryRepository coffeeChatScheduleQueryRepository;
 
-    public GetCoffeeChatScheduleUseCase(final CoffeeChatScheduleQueryRepository coffeeChatScheduleQueryRepository) {
+    public GetCoffeeChatScheduleUseCase(
+            final CoffeeChatRepository coffeeChatRepository,
+            final CoffeeChatScheduleQueryRepository coffeeChatScheduleQueryRepository
+    ) {
+        this.coffeeChatRepository = coffeeChatRepository;
         this.coffeeChatScheduleQueryRepository = coffeeChatScheduleQueryRepository;
     }
 
     public CoffeeChatEachCategoryCounts getEachCategoryCounts(final Authenticated authenticated) {
         if (authenticated.isMentor()) {
-            return CoffeeChatEachCategoryCounts.from(coffeeChatScheduleQueryRepository.fetchMentorCoffeeChatCountPerCategory(authenticated.id));
+            return new CoffeeChatEachCategoryCounts(
+                    coffeeChatRepository.getMentorWaitingCoffeeChatCount(authenticated.id),
+                    coffeeChatRepository.getMentorSuggestedCoffeeChatCount(authenticated.id),
+                    coffeeChatRepository.getMentorScheduledCoffeeChatCount(authenticated.id),
+                    coffeeChatRepository.getMentorPassedCoffeeChatCount(authenticated.id)
+            );
+//            return CoffeeChatEachCategoryCounts.from(coffeeChatScheduleQueryRepository.fetchMentorCoffeeChatCountPerCategory(authenticated.id));
         }
-        return CoffeeChatEachCategoryCounts.from(coffeeChatScheduleQueryRepository.fetchMenteeCoffeeChatCountPerCategory(authenticated.id));
+        return new CoffeeChatEachCategoryCounts(
+                coffeeChatRepository.getMenteeWaitingCoffeeChatCount(authenticated.id),
+                coffeeChatRepository.getMenteeSuggestedCoffeeChatCount(authenticated.id),
+                coffeeChatRepository.getMenteeScheduledCoffeeChatCount(authenticated.id),
+                coffeeChatRepository.getMenteePassedCoffeeChatCount(authenticated.id)
+        );
+//        return CoffeeChatEachCategoryCounts.from(coffeeChatScheduleQueryRepository.fetchMenteeCoffeeChatCountPerCategory(authenticated.id));
     }
 
     public SliceResponse<List<MentorCoffeeChatScheduleData>> getMentorSchedules(final GetMentorCoffeeChats query) {
