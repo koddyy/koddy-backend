d9e8351fec85a7c0e362c425e21341abe5cb4ff1
sjiwon <sjiwon4491@gmail.com>
2024-02-26 21:44:45 +0900

    fix: ApiDocsTest 관련 Controller Injection `@UseCase`에 대한 오류 수정

--- src/main/java/com/koddy/server/admin/controller/DummyAccountLoginApi.kt
+++ src/main/java/com/koddy/server/admin/controller/DummyAccountLoginApi.kt
@@ -1,17 +1,12 @@
 package com.koddy.server.admin.controller
 
+import com.koddy.server.admin.service.DummyAccountLoginService
 import com.koddy.server.admin.utils.NotProvidedInProduction
 import com.koddy.server.auth.domain.model.AuthToken
-import com.koddy.server.auth.domain.service.TokenIssuer
 import com.koddy.server.auth.utils.TokenResponseWriter
 import com.koddy.server.member.domain.model.Email
-import com.koddy.server.member.domain.model.Member
-import com.koddy.server.member.domain.repository.MemberRepository
-import com.koddy.server.member.exception.MemberException
-import com.koddy.server.member.exception.MemberExceptionCode.MEMBER_NOT_FOUND
 import jakarta.servlet.http.HttpServletResponse
 import jakarta.validation.constraints.NotBlank
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -21,10 +16,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/auth/dummy/login")
 class DummyAccountLoginApi(
-    private val memberRepository: MemberRepository,
-    private val tokenIssuer: TokenIssuer,
+    private val dummyAccountLoginService: DummyAccountLoginService,
     private val tokenResponseWriter: TokenResponseWriter,
-    @Value("\${account.dummy}") private val dummyAccountPassword: String,
 ) {
     data class LoginRequest(
         @field:NotBlank(message = "이메일은 필수입니다.")
@@ -40,14 +33,7 @@ class DummyAccountLoginApi(
         @RequestBody request: LoginRequest,
         response: HttpServletResponse,
     ): ResponseEntity<Void> {
-        val member: Member<*> = memberRepository.findByPlatformEmail(Email.from(request.email))
-            .orElseThrow { MemberException(MEMBER_NOT_FOUND) }
-
-        if (dummyAccountPassword != request.password) {
-            throw MemberException(MEMBER_NOT_FOUND)
-        }
-
-        val authToken: AuthToken = tokenIssuer.provideAuthorityToken(member.id, member.authority)
+        val authToken: AuthToken = dummyAccountLoginService.invoke(Email.from(request.email), request.password)
         tokenResponseWriter.applyToken(response, authToken)
         return ResponseEntity.ok().build()
     }
---
+++ src/main/java/com/koddy/server/admin/service/DummyAccountLoginService.kt
@@ -0,0 +1,32 @@
+package com.koddy.server.admin.service
+
+import com.koddy.server.auth.domain.model.AuthToken
+import com.koddy.server.auth.domain.service.TokenIssuer
+import com.koddy.server.global.annotation.UseCase
+import com.koddy.server.member.domain.model.Email
+import com.koddy.server.member.domain.model.Member
+import com.koddy.server.member.domain.repository.MemberRepository
+import com.koddy.server.member.exception.MemberException
+import com.koddy.server.member.exception.MemberExceptionCode.MEMBER_NOT_FOUND
+import org.springframework.beans.factory.annotation.Value
+
+@UseCase
+class DummyAccountLoginService(
+    private val memberRepository: MemberRepository,
+    private val tokenIssuer: TokenIssuer,
+    @Value("\${account.dummy}") private val dummyAccountPassword: String,
+) {
+    fun invoke(
+        email: Email,
+        password: String,
+    ): AuthToken {
+        val member: Member<*> = memberRepository.findByPlatformEmail(email)
+            .orElseThrow { MemberException(MEMBER_NOT_FOUND) }
+
+        if (dummyAccountPassword != password) {
+            throw MemberException(MEMBER_NOT_FOUND)
+        }
+
+        return tokenIssuer.provideAuthorityToken(member.id, member.authority)
+    }
+}
